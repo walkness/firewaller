7247bb7c375f778ebbaa649602d19cbdc5a5095d
Walker Angell <w@lker.co>
2017-11-20 15:09:17 -0500

    dont ignore messages

---
+++ app/scripts/messages/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/prefer-default-export */
+
+import { defineMessages } from 'react-intl';
+
+
+export const rules = defineMessages({
+  actions: {
+    id: 'rules.actions',
+    defaultMessage: `{action, select,
+      ACCEPT {accept}
+      DROP {drop}
+      REJECT {reject}
+      REJECT_TCP {reject TCP}
+      other {{action}}
+    }`,
+  },
+  number: {
+    id: 'rules.number',
+    defaultMessage: 'number',
+  },
+  description: {
+    id: 'rules.description',
+    defaultMessage: 'description',
+  },
+});
